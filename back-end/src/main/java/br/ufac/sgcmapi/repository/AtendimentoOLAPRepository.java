a87e56f8d4800a8b4a2839338b2ec68e16864a22
Aluízio Catão <aluizio2210@gmail.com>
2022-07-29 20:35:42 -0500

    Ultima Aula Parte 1

---
+++ back-end/src/main/java/br/ufac/sgcmapi/repository/AtendimentoOLAPRepository.java
@@ -0,0 +1,10 @@
+package br.ufac.sgcmapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufac.sgcmapi.model.AtendimentoOLAP;
+
+public interface AtendimentoOLAPRepository extends JpaRepository<AtendimentoOLAP, Long>{
+  
+  
+}
